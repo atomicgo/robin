55258f35830ea4cfcb8fa7080825c7233daddb98
MarvinJWendt <git@marvinjwendt.com>
2023-06-18 04:28:20 +0200

    examples: added demo example

--- examples_test.go
+++ examples_test.go
@@ -5,6 +5,38 @@ import (
 	"fmt"
 )
 
+type Person struct {
+	Name string
+}
+
+func ExampleLoadbalancer_demo() {
+	people := []Person{
+		{Name: "person1"},
+		{Name: "person2"},
+		{Name: "person3"},
+		{Name: "person4"},
+		{Name: "person5"},
+	}
+
+	lb := robin.NewLoadbalancer(people)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(lb.Next().Name)
+	}
+
+	// Output:
+	// person1
+	// person2
+	// person3
+	// person4
+	// person5
+	// person1
+	// person2
+	// person3
+	// person4
+	// person5
+}
+
 func ExampleNewLoadbalancer() {
 	set := []string{"object1", "object2", "object3"}
 	lb := robin.NewLoadbalancer(set)
